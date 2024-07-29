1e2b639d343daa14a02c1545399d3c4a913df4c4
Aryan Mishra <mishraaryan9204@gmail.com>
2024-07-29 10:09:04 +0530

    Learn Wrappers

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -1,7 +1,22 @@
+import { FeedWrapper } from "@/components/feedwrapper";
+import { StickyWrapper } from "@/components/stickywrapper";
+import { Header } from "./header";
+import { UserProgress } from "@/components/user-progress";
+
 const LearnPage = () => {
     return (
-        <div>
-            Learn page
+        <div className="flex flex-row-reverse gap-[48px] px-6">
+            <StickyWrapper >
+                <UserProgress 
+                    activeCourse={{title: "Spanish", imageSrc: "/es.svg"}}
+                    hearts={5}
+                    points={10}
+                    hasActiveSubscription={true}
+                />
+            </StickyWrapper>
+            <FeedWrapper>
+                <Header title="Spanish"/>
+            </FeedWrapper>
         </div>
     )
 }

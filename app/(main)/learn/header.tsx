f6b115525b878781c388d11d335c706f158cd92f
Aryan Mishra <mishraaryan9204@gmail.com>
2024-07-29 12:44:40 +0530

    courses page setup

--- app/(main)/learn/header.tsx
+++ app/(main)/learn/header.tsx
@@ -8,16 +8,16 @@ type Props = {
 
 export const Header = ({title} : Props) => {
     return (
-        <div className="sticky top-0 pb-3 lg:pt-[28px] lg:mt-[-28px] flex items-center justify-between border-b-2 mb-5 text-neutral-400 backdrop-blur-lg lg:z-50">
+        <div className="sticky top-0 pb-3 lg:pt-[28px] lg:mt-[-28px] flex items-center justify-between border-b-2 border-muted-foreground mb-5 text-neutral-400 backdrop-blur-lg lg:z-50">
             <Link href={'/courses'}>
                 <Button
                     variant={"ghost"}
                     size={'sm'}
                 >
-                    <ArrowLeft className="h-5 w-5 stroke-2 text-neutral-400"/>
+                    <ArrowLeft className="h-5 w-5 stroke-2 text-white"/>
                 </Button>
             </Link>
-            <h1 className="font-bold text-lg">
+            <h1 className="font-bold text-lg text-white">
                 {title}
             </h1>
             <div />
